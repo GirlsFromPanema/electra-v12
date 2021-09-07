bdc334da7aacfa5ca1c01f11b63560ad5bc27c94
Konrad570 <Konradtoesch@icloud.com>
2021-09-07 10:45:33 +0200

    Update

--- output/src/commands/servers.js
+++ output/src/commands/servers.js
@@ -1,6 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const classes_1 = require("../classes");
+const ReactionMenu = require("../Data/ReactionMenu")
+const MessageEmbed = require("discord.js")
+
 class Bot_name extends classes_1.Command {
   constructor(client) {
     super({
@@ -16,25 +19,37 @@ class Bot_name extends classes_1.Command {
     if (msg.author.id !== "578678204890349594") {
       return msg.channel.send(":x: | You are not allowed to use this Command");
     }
-
     const servers = msg.client.guilds.cache.array().map((guild) => {
       return `\`${guild.id}\` - **${guild.name}** - \`${guild.memberCount}\` members`;
     });
 
+    let embed = msg.channel.send({
+      title: "Server List",
+      color: "BLUE",
+      description: ``,
+      footer: {
+        text: msg.author.displayAvatarURL({ dynamic: true }),
+      }
+
+    })
+      
+
     if (servers.length <= 10) {
       const range = servers.length == 1 ? "[1]" : `[1 - ${servers.length}]`;
-      let embed = msg.channel.send({
-        embed: {
-          color: client.brandingColor,
+      let serverembed = msg.channel.send({
           title: `Server List ${range}`,
-          description: servers.join("\n"),
-
-          footer: {
-            text: "🌐 Powered by Fairfight",
-          },
-        },
+          description: servers.join("\n")
       });
+    } else {
+      new ReactionMenu(
+        msg.client,
+        msg.channel,
+        msg.member,
+        embed,
+        servers
+      );
     }
+    
   }
 }
 exports.default = Bot_name;
